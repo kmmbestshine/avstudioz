1f7b717fa02c222334abeeb64807eb88c54960e1
example1 <32572574+bestshine@users.noreply.github.com>
2020-09-11 12:17:13 +0530

    initial commit
    initial commit

---
+++ app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function index() {
+
+        $products = Product::inRandomOrder()->take(4)->get();
+
+        return view('front.index', compact('products'));
+    }
+    public function moredetails() {
+    	$input = \Request::all();
+    	
+        $products = Product::where('id', $input['id'])->get();
+        foreach ($products as $product) {
+            $mul_images=$product->mul_images;
+        }
+
+        $temp = explode('|',$mul_images );
+        
+        return view('front.moredetails', compact('products','temp'));
+    }
+}
